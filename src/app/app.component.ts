5bb69c61c0e181e13dbedaf4f8a2c38b56e5bb6c
Hossein <hosseincharchian@gmail.com>
2024-06-27 01:29:23 +0330

    navok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { navBarcomponent } from './navBar/navBar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet
+  , navBarcomponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/navBar/navBar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    templateUrl: './navBar.component.html',
+    styleUrl: './navBar.component.css',
+    standalone : true ,
+    selector : 'navB'
+
+})
+
+export class navBarcomponent {
+
+}
